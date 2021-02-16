c46e4e6817710b718b0b9bb0a49538accd83cf0e
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-02-16 13:37:05 +0100

    Added the library to control communication between java and SQLite. Made a package to store the console version of my mobilpay app

---
+++ src/consoleApp/MainConsoleTest.java
@@ -0,0 +1,21 @@
+package consoleApp;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MainConsoleTest {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
